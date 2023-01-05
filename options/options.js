cfa3d286b51e8713db37cf08610812ab1192944e
Crowdbotics <team@crowdbotics.com>
2023-01-05 01:38:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "music-match-38188",
+  url: "https://music-match-38188.botics.co",
+  api: "https://music-match-38188.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
